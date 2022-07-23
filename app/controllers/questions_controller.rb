1b30d3a159a9dd285aa0918faad4357bd7c0f7eb
Jie Lin Lee <lin.leejie@gmail.com>
2022-07-23 14:29:02 +0800

    completed stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = params[:answer]
+    if @answer == "I am going to work"
+      @response = "Great!"
+    elsif @answer.include?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "ask", to: "questions#ask"
+  post "answer", to: "questions#answer"
+
 end
